d4d4867f2352dd522d00c39275ed55c1f8602fc3
d0kur0 <dev.dokuro@gmail.com>
2020-02-03 00:36:21 +0300

    Add .idea dir, node_modules and build

---
+++ src/components/Disclaimer.svelte
@@ -0,0 +1,78 @@
+{#if !checked}
+<div class="disclaimer" transition:fade>
+  <div class="disclaimer__content">
+    <h1 class="disclaimer__header">Обратите внимание</h1>
+    <p class="disclaimer__description">
+      Весь контент на данном ресурсе взят с таких сайтов как: <a href="https://2ch.hk">2ch.hk</a>, <a href="https://4chan.org">4chan.org</a> и принадлежит его пользователям.
+    </p>
+    <p class="disclaimer__description">
+      Некоторые из "досок" могут содержать в себе файлы для взрослых, рачленёнку и тому подобное, если вам нет 18 лет или если вы слишком впечатлителен, то вам стоит покинуть сайт.
+    </p>
+
+    <button on:click={closeDisclaimer} class="disclaimer__continue">Перейти к сайту →</button>
+  </div>
+</div>
+{/if}
+
+<script>
+  import { fade } from 'svelte/transition';
+  
+  let checked = localStorage.checkedDisclaimer !== undefined;
+
+  function closeDisclaimer ()  {
+    localStorage.checkedDisclaimer = true;
+    checked = true;
+  }
+</script>
+
+<style>
+  .disclaimer {
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #fafafa;
+    display: flex;
+    align-items: center;
+  }
+
+  .disclaimer__content {
+    max-width: 600px;
+    width: 100%;
+    padding: 0 15px;
+    margin: auto;
+  }
+
+  .disclaimer__header {
+    margin: 0;
+    padding: 10px 0;
+    text-shadow: 1px 1px 1px #d8d8d8;
+  }
+
+  .disclaimer__description {
+    font-size: 1.1em;
+    color: #323232;
+    text-shadow: 1px 1px 1px #d8d8d8;
+  }
+
+  .disclaimer__continue {
+    font: inherit;
+    font-size: 1.2em;
+    cursor: pointer;
+    padding: 1px 0;
+    margin: 0;
+    margin-top: 15px;
+    border: none;
+    background-color: transparent;
+    color: #353535;
+  }
+
+  .disclaimer__continue:active {
+    background-color: transparent;
+  }
+
+  .disclaimer__continue:hover {
+    color: #000000;
+  }
+</style>
