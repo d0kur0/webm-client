6b9dc76f16f15318b75258afd42c6d90dbba64ec
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 15:22:22 +0300

    Fix meta tag for disable referrer

--- src/api.js
+++ src/api.js
@@ -1,12 +1,20 @@
 import axios from "axios";
+
 const apiAddress = "http://localhost:3500";
 
-export function getSchema () {
-	return axios.get(`${apiAddress}/schema/get`)
+export async function getSchema () {
+	const response = await axios.get(`${apiAddress}/schema/get`);
+
+	return Object.entries(response.data).map(vendor => {
+		return {
+			vendor: vendor[0],
+			boards: vendor[1]
+		}
+	});
 }
 
 export function getFiles () {
-	return axios.get(`${apiAddress}/files/getAll`)
+	return axios.get(`${apiAddress}/files/getAll`);
 }
 
 export function getFilesByStruct (struct) {
