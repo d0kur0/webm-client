67107fd0e274e3528619319685a41927504df589
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 15:26:52 +0300

    Implement component, first version

--- src/components/settings/Index.svelte
+++ src/components/settings/Index.svelte
@@ -15,7 +15,12 @@
 
 			{#if schema}
 				{#each schema as { vendor, boards, expanded }}
-					<Vendor vendor={vendor} boards={boards} expanded={expanded} />
+					<Vendor
+									on:toggleVendor={handleToggleVendor}
+									on:toggleBoard={handleToggleBoard}
+									{vendor}
+									{boards}
+									{expanded} />
 				{/each}
 			{:else}
 				Загрузка данных...
@@ -28,38 +33,78 @@
 	import Vendor from "./Vendor.svelte";
 	import { onMount } from "svelte";
 	import { getSchema } from "../../api";
+	import { getLocalSchema, saveLocalSchema, mergeSchemes } from "../../utils/localSchema";
 
-	let isOpen = true;
+	let isOpen = false;
+	let needUpdate = false;
+
+	const dispatchUpdateFiles = () => {
+		const event = new CustomEvent("updateFiles");
+		document.body.dispatchEvent(event);
+	};
+
 	const showModalWindow = () => isOpen = true;
-	const hideModalWindow = () => isOpen = false;
-	const handleEscapeKey = (e) => (e.which === 27) && hideModalWindow();
 
-	let schema;
-	onMount(() => {
-		getSchema().then(res => {
-			const serverSchema = Object.entries(res.data).map(function (vendor) {
-				const boards = vendor[1].map(function (board) {
-					board.disabled = false;
-					return board;
+	const hideModalWindow = () => {
+		isOpen = false;
+
+		if (needUpdate) {
+			dispatchUpdateFiles();
+			needUpdate = false;
+		}
+	};
+
+	const handleEscapeKey = e => {
+		if (e.which === 27) {
+			hideModalWindow();
+
+			if (needUpdate) {
+				dispatchUpdateFiles();
+				needUpdate = false;
+			}
+		}
+	};
+
+	const handleToggleVendor = e => {
+		schema = schema.map(v => {
+			if (v.vendor === e.detail) v.expanded = !v.expanded;
+
+			return v;
+		});
+
+		needUpdate = true;
+		saveLocalSchema(schema);
+	};
+
+	const handleToggleBoard = e => {
+		const { vendor, board } = e.detail;
+
+		schema = schema.map(v => {
+			if (v.vendor === vendor) {
+				v.boards = v.boards.map(b => {
+					if (b.name === board) b.disabled = !b.disabled;
+
+					return b;
 				});
-
-				return {
-					vendor: vendor[0],
-					expanded: true,
-					boards: boards
-				}
-			});
-
-			let schemaSettings = localStorage.schemaSettings;
-			if (!schemaSettings) schema = serverSchema;
-
-			try {
-				schemaSettings = JSON.parse(schemaSettings);
-			} catch  {
-				schema = serverSchema;
 			}
 
+			return v;
 		});
+
+		needUpdate = true;
+		saveLocalSchema(schema);
+	};
+
+	let schema;
+	onMount(async () => {
+		const serverSchema = await getSchema();
+		const localSchema = getLocalSchema();
+
+		schema = localSchema
+				? mergeSchemes(serverSchema)
+				: serverSchema;
+
+		(!localSchema) && saveLocalSchema(serverSchema);
 	});
 </script>
 
