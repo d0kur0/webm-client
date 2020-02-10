6c0cb8e20e47cc9a93cf50e3cb83ae0824be15fb
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 17:06:16 +0300

    Progress component

---
+++ src/components/videoPlayer/Progress.svelte
@@ -0,0 +1,20 @@
+<template>
+	<div class="progress-hack">
+		<input
+				step="any"
+				on:input={e => time.set(+e.target.value)}
+				style="--min: 0; --max: {$duration}; --val: {$time}"
+				type="range"
+				min="0"
+				max="{$duration}"
+				value="{$time}">
+	</div>
+</template>
+
+<script>
+	import { time, duration } from "../../stores/player";
+</script>
+
+<style lang="scss">
+	@import "./styles/progress.scss";
+</style>
