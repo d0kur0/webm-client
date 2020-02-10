22b760db84dea2fd6f128d266bfd19670d032302
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 15:23:03 +0300

    Add template and reident

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,25 @@
 <script>
-	import Disclaimer from "./components/Disclaimer.svelte";
-	import VideoArea from "./components/VideoArea.svelte";
-	import ThemeToggler from "./components/ThemeToggler.svelte";
+	import Disclaimer from "./components/disclaimer/Index.svelte";
+	import VideoArea from "./components/videoArea/Index.svelte";
+	import ThemeToggler from "./components/themeToggler/Index.svelte";
 	import Settings from "./components/settings/Index.svelte";
-	import WideScreen from "./components/WideScreen.svelte"
+	import WideScreen from "./components/wideScreen/Index.svelte"
 </script>
 
-<Disclaimer />
+<template>
+	<Disclaimer />
 
-<header>
-    <div>
-        <Settings />
-		    <WideScreen />
-    </div>
-    <ThemeToggler />
-</header>
-<main>
-  <VideoArea />
-</main>
+	<header>
+		<div>
+			<Settings />
+			<WideScreen />
+		</div>
+		<ThemeToggler />
+	</header>
+	<main>
+		<VideoArea />
+	</main>
+</template>
 
 <style lang="scss">
 		main {
