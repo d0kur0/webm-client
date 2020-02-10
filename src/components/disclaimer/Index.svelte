a2b3ad2299eec03515a4da9f5728086287ac8c4b
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 15:26:06 +0300

    Change description, add template and refactor styles

--- src/components/disclaimer/Index.svelte
+++ src/components/disclaimer/Index.svelte
@@ -1,18 +1,26 @@
-{#if !checked}
-<div class="disclaimer" transition:fade>
-  <div class="disclaimer__content">
-    <h1 class="disclaimer__header">Обратите внимание</h1>
-    <p class="disclaimer__description">
-      Весь контент на данном ресурсе взят с таких сайтов как: <a href="https://2ch.hk">2ch.hk</a>, <a href="https://4chan.org">4chan.org</a> и принадлежит его пользователям.
-    </p>
-    <p class="disclaimer__description">
-      Некоторые из "досок" могут содержать в себе файлы для взрослых, рачленёнку и тому подобное, если вам нет 18 лет или если вы слишком впечатлителен, то вам стоит покинуть сайт.
-    </p>
+<template>
+  {#if !checked}
+  <div class="container" transition:fade>
+    <div class="content">
+      <h1 class="header">Обратите внимание</h1>
 
-    <button on:click={closeDisclaimer} class="disclaimer__continue">Перейти к сайту →</button>
+      <p class="description">
+        Весь контент на данном ресурсе взят с таких сайтов как: <a href="https://2ch.hk">2ch.hk</a>, <a href="https://4chan.org">4chan.org</a> и принадлежит его пользователям.
+      </p>
+
+      <p class="description">
+        Некоторые из "досок" могут содержать в себе файлы для взрослых, рачленёнку и тому подобное.
+        Хоть показываемые доски и можно настроить, это не исключает возможности просмотра возможно не самого приятного контента.
+      </p>
+      <p class="description">
+        Если вам нет 18 лет или если вы слишком впечатлителен, то вам стоит покинуть сайт.
+      </p>
+
+      <button on:click={closeDisclaimer} class="continue-button">Перейти к сайту →</button>
+    </div>
   </div>
-</div>
-{/if}
+  {/if}
+</template>
 
 <script>
   import { fade } from 'svelte/transition';
@@ -25,56 +33,4 @@
   }
 </script>
 
-<style>
-  .disclaimer {
-    background-color: var(--main-background);
-    z-index: 1;
-    width: 100vw;
-    height: 100vh;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-  }
-
-  .disclaimer__content {
-    max-width: 600px;
-    width: 100%;
-    padding: 0 15px;
-    margin: auto;
-  }
-
-  .disclaimer__header {
-    color: var(--ancent-color-light);
-    margin: 0;
-    padding: 10px 0;
-    text-shadow: 1px 1px 1px var(--text-shadow-color);
-  }
-
-  .disclaimer__description {
-    font-size: 1.1em;
-    color: var(--ancent-color-light);
-    text-shadow: 1px 1px 1px var(--text-shadow-color);
-  }
-
-  .disclaimer__continue {
-    font: inherit;
-    font-size: 1.2em;
-    cursor: pointer;
-    padding: 1px 0;
-    margin: 0;
-    margin-top: 15px;
-    border: none;
-    background-color: transparent;
-    color: var(--ancent-color-light);
-  }
-
-  .disclaimer__continue:active {
-    background-color: transparent;
-  }
-
-  .disclaimer__continue:hover {
-    color: var(--ancent-color-dark);
-  }
-</style>
+<style src="./styles/disclaimer.scss"></style>
