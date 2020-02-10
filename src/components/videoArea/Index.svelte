38d57979c2f9755db29af3bb9f83dd0253169a97
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 17:01:25 +0300

    Implode logic with components for video and controls

--- src/components/videoArea/Index.svelte
+++ src/components/videoArea/Index.svelte
@@ -1,5 +1,12 @@
+<svelte:body on:updateFiles={handleUpdateFiles}></svelte:body>
+
 <div class="video-area">
-	<VideoPlayer />
+	<VideoPlayer
+					on:error={() => removeBadVideo() + nextVideo() }
+					on:ended={nextVideo}
+					on:previous={previousVideo}
+					on:next={nextVideo}
+					{video} />
 </div>
 
 <style>
@@ -12,5 +19,42 @@
 </style>
 
 <script>
-	import VideoPlayer from "./videoPlayer/Index.svelte";
+	import VideoPlayer from "../videoPlayer/Index.svelte";
+	import { getLocalSchema } from "../../utils/localSchema";
+	import { onMount } from "svelte"
+	import { getFilesByStruct } from "../../api";
+
+	let cursor = 0;
+	let files = [];
+	let video;
+
+	const removeBadVideo = () => {
+		files = files.splice(cursor - 1, 1);
+		console.log('remove');
+	};
+
+	const previousVideo = () => {
+		if (cursor - 1 >= 0) {
+			cursor -= 1;
+			video = files[cursor];
+		}
+	};
+
+	const nextVideo = () => {
+		console.log('next');
+		if (cursor + 1 <= files.length) {
+			cursor += 1;
+			video = files[cursor];
+			console.log('next 2', cursor, video);
+		}
+	};
+
+	const fetchFiles = async () => {
+		files = await getFilesByStruct(getLocalSchema());
+		video = files.shift();
+	};
+
+	const handleUpdateFiles = async () => await fetchFiles();
+
+	onMount(async () => await fetchFiles());
 </script>
