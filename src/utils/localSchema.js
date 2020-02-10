53a40b5123099238179402b76f493538e2bdfcf0
d0kur0 <dev.dokuro@gmail.com>
2020-02-10 17:06:44 +0300

    Add util for work with localSchema

---
+++ src/utils/localSchema.js
@@ -0,0 +1,42 @@
+export function getLocalSchema () {
+	if (!localStorage.localSchema) return false;
+
+	let localSchema;
+	try {
+		localSchema = JSON.parse(localStorage.localSchema);
+	} catch {
+		return false;
+	}
+
+	return localSchema;
+}
+
+export function saveLocalSchema (schema) {
+	localStorage.localSchema = JSON.stringify(schema);
+}
+
+export function mergeSchemes (serverSchema) {
+	const localSchema = getLocalSchema();
+
+	const getLocalVendor = vendor => localSchema.filter(v => v.vendor === vendor)[0];
+	const getLocalBoard = (vendor, board) => {
+		const localVendor = getLocalVendor(vendor);
+		if (!localVendor) return false;
+
+		return localVendor.boards.filter(b => b.name === board)[0];
+	};
+
+	return serverSchema.map(schema => {
+		const localVendor = getLocalVendor(schema.vendor);
+		if (localVendor) schema.expanded = localVendor.expanded;
+
+		schema.boards = schema.boards.map(board => {
+			const localBoard = getLocalBoard(schema.vendor, board.name);
+			if (localBoard) board.disabled = localBoard.disabled;
+
+			return board;
+		});
+
+		return schema;
+	});
+}
