aa978e48f874b51fc54873636f04f9f9b39ca188
d0kur0 <dev.dokuro@gmail.com>
2020-02-15 23:08:49 +0300

    Rewrite with ui-components

--- src/components/themeToggler/Index.svelte
+++ src/components/themeToggler/Index.svelte
@@ -1,50 +1,11 @@
-<button class="theme-toggler" aria-label="Изменить тему сайта" on:click={toggle}>
-	{#if isNight}
-		<svg  width="100%" height="100%" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-			<g fill="none" style="stroke: var(--ancent-color)" stroke-miterlimit="10" stroke-width="30">
-				<circle cx="255.899" cy="256.45" r="105.706"/>
-				<path d="M255 58v57M56 256h58M399 256h57M255 397v57M355.034 159.118l40.77-40.77M355.034 356.186l40.77 40.77M109.989 396.956l40.77-40.77M109.989 118.348l40.77 40.77"/>
-			</g>
-		</svg>
-	{:else}
-		<svg width="100%" height="100%" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-			<path
-					d="M386 248.478c0 78.864-63.941 142.805-142.805 142.805-48.513 0-91.395-24.202-117.195-61.194 14.54 4.687 30.037 7.215 46.135 7.215 83.018 0 150.321-67.303 150.321-150.321 0-23.778-5.521-46.258-15.346-66.237C353.886 144.197 386 192.587 386 248.478z"
-					fill="none"
-					style="stroke: var(--ancent-color)"
-					stroke-linecap="round"
-					stroke-miterlimit="10"
-					stroke-width="30"/>
-		</svg>
-	{/if}
-</button>
-
-<style lang="scss">
-	.theme-toggler {
-		width: 32px;
-		height: 32px;
-		margin: 0;
-		padding: 0;
-		border: none;
-		background-color: transparent;
-		cursor: pointer;
-		filter: opacity(70%);
-		transition: 0.3s;
-		display: flex;
-		align-items: center;
-
-		&:hover {
-			filter: opacity(100%);
-		}
-
-		svg {
-			width: 100%;
-			height: 100%;
-		}
-	}
-</style>
+<Button aria-label="Изменить тему сайта" on:click={toggle}>
+	<Icon size="" iconName="{isNight ? `sun` : `moon`}" />
+</Button>
 
 <script>
+	import Icon from "../../ui-elements/Icon.svelte";
+	import Button from "../../ui-elements/Button.svelte";
+
 	let isNight = localStorage.isNightTheme !== undefined;
 	if (isNight) document.body.classList.add('dark-theme');
 
