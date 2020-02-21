35822e70f33ae4f723b963ee4b6f03704a19b240
d0kur0 <dev.dokuro@gmail.com>
2020-02-21 09:08:39 +0300

    Add store for videos

---
+++ src/stores/videos.js
@@ -0,0 +1,45 @@
+import {writable} from 'svelte/store';
+
+function createVideosStore() {
+	const { subscribe, set, update } = writable({
+		files: [],
+		currentVideo: undefined,
+		index: 0
+	});
+
+
+	return {
+		subscribe,
+		set,
+		save (videos) {
+			update(store => {
+				store.files = videos;
+				store.currentVideo = store.files[store.index];
+
+				return store;
+			});
+		},
+		next () {
+			update(store => {
+				if (store.index + 1 > store.files.length) return store;
+
+				store.index += 1;
+				store.currentVideo = store.files[store.index];
+
+				return store;
+			});
+		},
+		previous () {
+			update(store => {
+				if (store.index - 1 < 0) return store;
+
+				store.index -= 1;
+				store.currentVideo = store.files[store.index];
+
+				return store;
+			});
+		}
+	};
+}
+
+export const videos = createVideosStore();
