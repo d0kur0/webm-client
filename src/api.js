2bc0c3da3be55ea39270303289adae75151dc705
d0kur0 <dev.dokuro@gmail.com>
2020-02-24 03:12:40 +0300

    Refactoring, fix bugs

--- src/api.js
+++ src/api.js
@@ -3,42 +3,67 @@ import axios from "axios";
 const apiAddress = "http://localhost:3500";
 
 export async function getSchema () {
-	const response = await axios.get(`${apiAddress}/schema/get`);
+	let response;
+	try {
+		response = await axios.get(`${apiAddress}/schema/get`);
+	} catch (e) {
+		console.log("Error load schema");
+		return false;
+	}
 
-	return Object.entries(response.data).map(vendor => {
+	return Object.entries(response.data).map(schema => {
+		const [ vendor, boards ] = schema;
+
 		return {
-			vendor: vendor[0],
+			vendor: vendor,
 			expanded: true,
-			boards: vendor[1].map(board => {
-				board.disabled = false;
-				return board;
-			})
+			boards: boards.map(board => ({...board, disabled: false}))
 		}
 	});
 }
 
 export async function getFilesByStruct (struct) {
-	struct = struct || getSchema();
+	struct = struct || await getSchema();
+	if (!struct) return false;
 
 	let requestStruct = { vendors: {} };
+
 	struct.forEach(schema => {
 		if (schema.expanded) {
-			requestStruct.vendors[schema.vendor] = schema.boards.filter(board => !board.disabled).map(board => board.name)
+			requestStruct.vendors[schema.vendor] = schema.boards.
+				filter(board => !board.disabled).
+				map(board => board.name)
 		}
 	});
 
 	let files = [];
-	let response = await axios.post(`${apiAddress}/files/getByStruct`, JSON.stringify(requestStruct));
-	Object.entries(response.data.vendors).map(s => s[1] && s[1].map(b => b.threads.map(t => t.files.map(f => {
-		files.push({
-			name:     f.name,
-			path:     f.path,
-			preview:  f.preview,
-			threadId: f.threadId,
-			board:    b.name,
-			vendor:   s[0]
+	let response;
+
+	try {
+		response = await axios.post(`${apiAddress}/files/getByStruct`, JSON.stringify(requestStruct));
+	} catch (e) {
+		return Promise.reject(new Error("Error load files by struct"));
+	}
+
+	Object.entries(response.data.vendors).forEach(schema => {
+		const [ vendor, boards ] = schema;
+
+		if (!boards) return;
+		boards.forEach(board => {
+			board.threads.forEach(thread => {
+				thread.files.forEach(file => {
+					files.push({
+						name:     file.name,
+						path:     file.path,
+						board:    board.name,
+						vendor:   vendor,
+						preview:  file.preview,
+						threadId: file.threadId
+					});
+				});
+			});
 		});
-	}))));
+	});
 
 	let currentIndex = files.length;
 	let temporaryValue;
